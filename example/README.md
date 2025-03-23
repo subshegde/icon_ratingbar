548cf0caef26b584e66425f2cd016e3f287d106c
Subrahmanya S Hegde <125819438+subshegde@users.noreply.github.com>
2025-03-23 13:07:40 +0530

    readme

--- example/README.md
+++ example/README.md
@@ -1,16 +1,125 @@
-# example
+# Icon Rating Bar
 
-A new Flutter project.
+**Overview**: 
+  - The `IconRatingBar` is a customizable Flutter widget that allows users to select a rating by tapping on icons, such as stars. It provides a flexible and highly customizable way of implementing a rating system, which can be visually enhanced by altering colors, icon types, and the appearance of the container holding the rating icons.
+
+- This widget supports both solid colors and gradients for the selected state, provides customizable spacing, container styling, and various design configurations. It makes it easy for developers to integrate a rating system into their apps, whether it’s for user feedback, product ratings, or any other scenario requiring a visual representation of user ratings.
+
+**Key Features :**
+
+*Customizable Icon Selection :*
+-  Allows the selection of different icons for the selected and unselected states, using IconData (e.g., stars, hearts, etc.).
+- Supports both solid colors and gradient effects for the selected icons.
+
+*Customizable Appearance :*
+- Customize the appearance of the rating container, including background color/gradient, shadow color, border color, border width, and border radius.
+- The widget allows setting custom dimensions for the container’s width, height, and icon size.
+
+*Gap Customization :*
+- Adjustable gap between icons to provide flexibility for design and layout.
+
+*Responsive Rating Updates :*
+- The rating is updated dynamically when an icon is tapped, and the parent widget is notified through the `onRatingChanged` callback.
+
+*Initial Rating :*
+- Set an initial rating value, which can be used for pre-selected ratings, such as showing a previous rating or defaulting to 0.
+
+*Customizable Color Scheme :*
+- Easily configure the selected and unselected colors (or gradients) for the icons. Unselected icons can be displayed in a default color like grey, while selected icons can be highlighted with a color or gradient like yellow.
+
+*Container Styling :*
+- The widget allows you to customize the background of the container that holds the icons (with solid color or gradient), making it blend well with the overall app design.
+
+*Shadow and Border Support :*
+- The container of the rating bar can have a shadow and border styling, helping to elevate its appearance visually on the screen.
+
+
+## About The Package
+
+The `IconRatingBar` widget is designed for use in Flutter applications to implement an easy-to-use, customizable rating interface. This widget was created to address the need for a flexible and visually appealing rating system that can easily adapt to different design themes.
+
+The widget leverages Flutter’s robust UI-building capabilities and provides developers with a powerful way to add interactive ratings into their applications without much overhead. Whether it's for a product rating system, user feedback on services, or a simple rating scale in any app, this widget offers a simple and intuitive solution.
+
+## Use Cases
+
+`Product Ratings` : Let users rate products or services with star-based ratings.
+
+`Feedback Systems` : Collect ratings for apps or features within an app.
+
+`Survey/Forms` : Use it in surveys where users rate their experience on a scale.
+
+`User Engagement` : Enhance user interaction by making ratings fun with custom icons or gradients.
+
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+To use the `icon_ratingbar` package in your Flutter project, follow these steps:
 
-A few resources to get you started if this is your first Flutter project:
+### Prerequisites
+Ensure you have the following installed on your system:
+* [Flutter](https://flutter.dev/docs/get-started/install)
+* [Dart](https://dart.dev/get-dart)
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+### Installation
+1. Add the package dependency in your `pubspec.yaml` file:
+    ```yaml
+    dependencies:
+      icon_ratingbar:
+    ```
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+2. Install the dependencies by running the following command:
+    ```bash
+    flutter pub get
+    ```
+
+3. Import the package into your Dart file:
+    ```dart
+    import 'package:icon_ratingbar/icon_ratingbar.dart';
+    ```
+
+## Usage
+
+Here’s how to integrate the `IconRatingBar` widget into your Flutter app:
+
+## Demo Video
+[![YouTube](https://img.shields.io/badge/-YouTube-black.svg?style=for-the-badge&logo=youtube&colorB=555)](https://youtu.be/GQJImcnQNuM?si=UtnxLdpp6uZqGa3R)
+
+
+```dart
+IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
+                totalStars: 5,
+                onRatingChanged: (rating) {},
+                selectedIcon: Icons.favorite,
+                unselectedIcon: Icons.favorite_border,
+                selectedColor: const LinearGradient(
+                  colors: [Colors.orange, Colors.pink],
+                ),
+                unselectedColor: Colors.grey,
+                containerBackgroundColor: Colors.white,
+                shadowColor: Colors.black.withOpacity(0.3),
+                borderColor: Colors.red,
+              ),
+```
+## 🚀 About Me
+I'm Subrahmanya S. Hegde, a mobile app developer with expertise in Flutter and React Native, passionate about building high-performance, cross-platform applications. With skills in Kotlin, Node.js, Firebase, and Supabase, I create end-to-end solutions that deliver seamless user experiences.
+
+I hold an MCA degree from MIT Manipal and have a strong passion for exploring new technologies. I am also a strong advocate of using Git and GitHub for version control and collaboration.
+
+Let’s build something great together! 
+#### Happy Coding!
+
+
+## Authors
+
+- [@subshegde](https://www.github.com/subshegde)
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at subrahmanya460@gmail.com
+
+#### My Github
+[![GitHub](https://img.shields.io/badge/-GitHub-black.svg?style=for-the-badge&logo=github&colorB=000000&colorA=333333)](https://github.com/subshegde)
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,13 +24,6 @@ class RatingPage extends StatefulWidget {
 }
 
 class _RatingPageState extends State<RatingPage> {
-  int _rating1 = 0;
-  int _rating2 = 0;
-  int _rating3 = 0;
-  int _rating4 = 0;
-  int _rating5 = 0;
-  int _rating6 = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,21 +40,20 @@ class _RatingPageState extends State<RatingPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Heart Icon - Normal
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating1 = rating;
-                  });
                 },
                 selectedIcon: Icons.favorite,
                 unselectedIcon: Icons.favorite_border,
-                initialRating: _rating1.toDouble(),
-                selectedColor: Colors.red, // Normal Red
-                unselectedColor: Colors.grey, // Grey for unselected
+                initialRating: 0,
+                selectedColor: Colors.red,
+                unselectedColor: Colors.grey,
                 gap: 10.0,
                 containerBackgroundColor: Colors.white,
                 shadowColor: Colors.black26,
@@ -69,41 +61,38 @@ class _RatingPageState extends State<RatingPage> {
                 borderRadius: 16.0,
                 borderWidth: 1.0,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Heart Icon - Gradient
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating5 = rating;
-                  });
                 },
                 selectedIcon: Icons.favorite,
                 unselectedIcon: Icons.favorite_border,
                 selectedColor: const LinearGradient(
-                  colors: [Colors.orange, Colors.pink], // Gradient for selected
+                  colors: [Colors.orange, Colors.pink],
                 ),
-                unselectedColor: Colors.grey, // Normal Grey for unselected
+                unselectedColor: Colors.grey,
                 containerBackgroundColor: const LinearGradient(colors: [Colors.white, Colors.white]),
                 shadowColor: Colors.black.withOpacity(0.3),
                 borderColor: Colors.red,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Star Icon - Normal
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating2 = rating;
-                  });
                 },
                 selectedIcon: Icons.star,
                 unselectedIcon: Icons.star_border,
-                initialRating: _rating2.toDouble(),
-                selectedColor: const Color.fromARGB(255, 237, 214, 4), // Normal Yellow
-                unselectedColor: Colors.grey, // Grey for unselected
+                selectedColor: const Color.fromARGB(255, 237, 214, 4),
+                unselectedColor: Colors.grey,
                 gap: 10.0,
                 containerBackgroundColor: Colors.white,
                 shadowColor: Colors.black26,
@@ -111,41 +100,38 @@ class _RatingPageState extends State<RatingPage> {
                 borderRadius: 16.0,
                 borderWidth: 2.0,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Star Icon - Gradient
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating2 = rating;
-                  });
                 },
                 selectedIcon: Icons.star,
                 unselectedIcon: Icons.star_border,
                 selectedColor: const LinearGradient(
-                  colors: [Colors.yellow, Colors.pink], // Gradient for selected
+                  colors: [Colors.yellow, Colors.pink],
                 ),
-                unselectedColor: Colors.grey, // Normal Grey for unselected
+                unselectedColor: Colors.grey,
                 containerBackgroundColor: const LinearGradient(colors: [Colors.white, Colors.white]),
                 shadowColor: Colors.black.withOpacity(0.3),
                 borderColor: Colors.white,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Car Icon - Normal
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating3 = rating;
-                  });
                 },
                 selectedIcon: Icons.car_rental,
                 unselectedIcon: Icons.car_rental_outlined,
-                initialRating: _rating3.toDouble(),
-                selectedColor: Colors.blue, // Normal Blue
-                unselectedColor: Colors.grey, // Grey for unselected
+                selectedColor: Colors.blue,
+                unselectedColor: Colors.grey,
                 gap: 10.0,
                 containerBackgroundColor: Colors.white,
                 shadowColor: Colors.black26,
@@ -153,41 +139,39 @@ class _RatingPageState extends State<RatingPage> {
                 borderRadius: 16.0,
                 borderWidth: 1.0,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Car Icon - Gradient
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating3 = rating;
-                  });
                 },
                 selectedIcon: Icons.car_rental,
                 unselectedIcon: Icons.car_rental_outlined,
                 selectedColor: const LinearGradient(
-                  colors: [Colors.blue, Colors.green], // Gradient for selected
+                  colors: [Colors.blue, Colors.green],
                 ),
-                unselectedColor: Colors.grey, // Normal Grey for unselected
+                unselectedColor: Colors.grey,
                 containerBackgroundColor: const LinearGradient(colors: [Colors.white, Colors.white]),
                 shadowColor: Colors.black.withOpacity(0.3),
                 borderColor: Colors.blue,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Sun Icon - Normal
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating4 = rating;
-                  });
                 },
                 selectedIcon: Icons.wb_sunny,
                 unselectedIcon: Icons.wb_sunny_outlined,
-                initialRating: _rating4.toDouble(),
-                selectedColor: Colors.orange, // Normal Orange
-                unselectedColor: Colors.grey, // Grey for unselected
+                initialRating: 2,
+                selectedColor: Colors.orange,
+                unselectedColor: Colors.grey,
                 gap: 10.0,
                 containerBackgroundColor: Colors.white,
                 shadowColor: Colors.black26,
@@ -195,27 +179,26 @@ class _RatingPageState extends State<RatingPage> {
                 borderRadius: 16.0,
                 borderWidth: 1.0,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
 
-              // Sun Icon - Gradient
               IconRatingBar(
+                iconSize: 20,
+                containerWidth: 200,
+                containerHeight: 40,
                 totalStars: 5,
                 onRatingChanged: (rating) {
-                  setState(() {
-                    _rating4 = rating;
-                  });
                 },
                 selectedIcon: Icons.wb_sunny,
                 unselectedIcon: Icons.wb_sunny_outlined,
                 selectedColor: const LinearGradient(
-                  colors: [Colors.orange, Colors.yellow], // Gradient for selected
+                  colors: [Colors.orange, Colors.yellow],
                 ),
-                unselectedColor: Colors.grey, // Normal Grey for unselected
+                unselectedColor: Colors.grey,
                 containerBackgroundColor: const LinearGradient(colors: [Colors.white, Colors.white]),
                 shadowColor: Colors.black.withOpacity(0.3),
                 borderColor: Colors.orange,
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(height: 10),
             ],
           ),
         ),
